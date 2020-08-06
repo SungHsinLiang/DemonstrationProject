25dc71ff431d13e0da0c0ad0edae7e6e486acd1a
Sung, Hsin-Liang <sunghsinliang@gmail.com>
2020-08-07 03:17:47 +0800

    update purchase cart show.vue

--- resources/views/purchase_cart/show.blade.php
+++ resources/views/purchase_cart/show.blade.php
@@ -6,7 +6,7 @@
 
 
 <purchase-cart-show :purchase_lists="{{ $purchase_lists }}" purchase_cart_route="{{ route('purchase_cart.index') }}"
-  product_route="{{ route('product.index')}}">
+  product_route="{{ route('product.index')}}" order_route="{{ route("order.store")}}">
 </purchase-cart-show>
 
 @endsection
